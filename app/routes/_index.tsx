616830729879b896da0a662205ab6247cc70259c
Simon Legg <leggsimon@gmail.com>
2024-05-19 15:28:21 +0200

    use tabs

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,33 +1,36 @@
 import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: '🏓 Table Tennis Tracker' },
-    { name: 'description', content: 'Track your ping pong games with your friends!' },
-  ];
+	return [
+		{ title: '🏓 Table Tennis Tracker' },
+		{
+			name: 'description',
+			content: 'Track your ping pong games with your friends!',
+		},
+	];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a target='_blank' href='https://remix.run/tutorials/blog' rel='noreferrer'>
-            We only deploy commited code now
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/tutorials/jokes' rel='noreferrer'>
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	return (
+		<div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+			<h1>Welcome to Remix</h1>
+			<ul>
+				<li>
+					<a target='_blank' href='https://remix.run/tutorials/blog' rel='noreferrer'>
+						We only deploy commited code now
+					</a>
+				</li>
+				<li>
+					<a target='_blank' href='https://remix.run/tutorials/jokes' rel='noreferrer'>
+						Deep Dive Jokes App Tutorial
+					</a>
+				</li>
+				<li>
+					<a target='_blank' href='https://remix.run/docs' rel='noreferrer'>
+						Remix Docs
+					</a>
+				</li>
+			</ul>
+		</div>
+	);
 }
