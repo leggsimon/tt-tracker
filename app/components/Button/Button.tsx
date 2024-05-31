84ece2b731f3c453cc5a394d95942ed4d9dd29c1
Simon Legg <leggsimon@gmail.com>
2024-05-31 11:27:52 +0200

    fix Button component Polymorphic typings

--- app/components/Button/Button.tsx
+++ app/components/Button/Button.tsx
@@ -1,13 +1,21 @@
-type ButtonProps = React.ComponentProps<'button'> & {
-	as?: React.ElementType;
-};
-export function Button({
+// Define the base props for the Button component
+interface BaseProps {
+	children: React.ReactNode;
+}
+
+// Create a utility type for merging props
+type PolymorphicProps<E extends React.ElementType, P> = P & {
+	as?: E;
+} & Omit<React.ComponentPropsWithoutRef<E>, keyof P>;
+
+export function Button<C extends React.ElementType = 'button'>({
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	className,
 	children,
-	as: Component = 'button',
+	as,
 	...props
-}: ButtonProps) {
+}: PolymorphicProps<C, BaseProps>) {
+	const Component = as || 'button';
 	return (
 		<Component
 			{...props}
